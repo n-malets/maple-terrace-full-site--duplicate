18fc7458b166086757f80ac10ff79da61bd3bd10
Pavlo Chorniy <pchorniy@proffiz.com>
2022-07-29 02:02:38 +0200

    building floor fix

--- src/components/building/content-floor-plans/index.tsx
+++ src/components/building/content-floor-plans/index.tsx
@@ -163,75 +163,79 @@ const ContentFloorPlans = () => {
     <FPWrapper>
       <FloorPlanImg ref={floorPlanImgRef}>
         <div className={"fp_images_wrapper"}>
-          <img
-            className={"fp_image_thumb"}
-            src={FloorPlansData.floorPlan1.img}
-            alt="Hines Maple-Terrace Floor-Plan L4"
-          />
-          <img
-            ref={fpImg1Ref}
-            className={"fp_image_1"}
-            src={FloorPlansData.floorPlan1.img}
-            alt="Hines Maple-Terrace Floor-Plan L4"
-          />
-          <img
-            ref={fpImg2Ref}
-            className={"fp_image_2"}
-            src={FloorPlansData.floorPlan2.img}
-            alt="Hines Maple-Terrace Floor-Plan L4"
-          />
-          <img
-            ref={fpImg3Ref}
-            className={"fp_image_3"}
-            src={FloorPlansData.floorPlan3.img}
-            alt="Hines Maple-Terrace Floor-Plan L4"
-          />
-          <div className="label label_top">MAPLE AVENUE</div>
-          <div className="label label_right">WOLF STREET</div>
-          <div className="label label_bottom">SECRET GARDEN</div>
-          <div className="level_labels" ref={levelLabelsRef}>
-            <a className={"active"}>Level 4</a>
-            <a>Level 5</a>
-            <a>Level 6</a>
+          <div className="fp_images_subwrapper">
+            <img
+              className={"fp_image_thumb"}
+              src={FloorPlansData.floorPlan1.img}
+              alt="Hines Maple-Terrace Floor-Plan L4"
+            />
+            <img
+              ref={fpImg1Ref}
+              className={"fp_image_1"}
+              src={FloorPlansData.floorPlan1.img}
+              alt="Hines Maple-Terrace Floor-Plan L4"
+            />
+            <img
+              ref={fpImg2Ref}
+              className={"fp_image_2"}
+              src={FloorPlansData.floorPlan2.img}
+              alt="Hines Maple-Terrace Floor-Plan L4"
+            />
+            <img
+              ref={fpImg3Ref}
+              className={"fp_image_3"}
+              src={FloorPlansData.floorPlan3.img}
+              alt="Hines Maple-Terrace Floor-Plan L4"
+            />
+
+            <a href={"#"} className="download_fit">
+              <svg
+                width="9"
+                height="12"
+                viewBox="0 0 9 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0 5.64829H2.58621V0H6.36207V5.68966H8.79311L4.38622 10.7793L0 5.64829Z"
+                  fill="white"
+                />
+                <path
+                  d="M8.82422 12V11.369H7.53403e-05V12H8.82422Z"
+                  fill="white"
+                />
+              </svg>
+              DOWNLOAD TEST FIT
+            </a>
           </div>
-          <a href={"#"} className="download_fit">
-            <svg
-              width="9"
-              height="12"
-              viewBox="0 0 9 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 5.64829H2.58621V0H6.36207V5.68966H8.79311L4.38622 10.7793L0 5.64829Z"
-                fill="white"
-              />
-              <path
-                d="M8.82422 12V11.369H7.53403e-05V12H8.82422Z"
-                fill="white"
-              />
-            </svg>
-            DOWNLOAD TEST FIT
-          </a>
-          <a href={"#"} className="download_All">
-            <svg
-              width="9"
-              height="12"
-              viewBox="0 0 9 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 5.64829H2.58621V0H6.36207V5.68966H8.79311L4.38622 10.7793L0 5.64829Z"
-                fill="white"
-              />
-              <path
-                d="M8.82422 12V11.369H7.53403e-05V12H8.82422Z"
-                fill="white"
-              />
-            </svg>
-            DOWNLOAD ALL
-          </a>
+          <div className="label label_top">MAPLE AVENUE</div>
+          <div className="label label_bottom">SECRET GARDEN</div>
+          <div className="level_all">
+            <div className="level_labels" ref={levelLabelsRef}>
+              <a className={"active"}>Level 4</a>
+              <a>Level 5</a>
+              <a>Level 6</a>
+            </div>
+            <a href={"#"} className="download_All">
+              <svg
+                width="9"
+                height="12"
+                viewBox="0 0 9 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0 5.64829H2.58621V0H6.36207V5.68966H8.79311L4.38622 10.7793L0 5.64829Z"
+                  fill="white"
+                />
+                <path
+                  d="M8.82422 12V11.369H7.53403e-05V12H8.82422Z"
+                  fill="white"
+                />
+              </svg>
+              DOWNLOAD ALL
+            </a>
+          </div>
         </div>
       </FloorPlanImg>
       <FloorPlan className="panel">
